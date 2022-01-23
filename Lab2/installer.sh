c4388f22c819451145c9289b98494ae7c2882d48
Mike <mkuimovk@mail.ru>
2022-01-23 12:32:00 +0300

    Update Lab2

---
+++ Lab2/installer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo cp ./name_saver.py /usr/bin/name_saver.py
+sudo chmod 755 /usr/bin/name_saver.py
+mkdir ~/namesaver/ || echo "Already installed"
+if ! test -f ~/.co
+ then touch ~/.co
+echo "4/n30" > ~/.co;
+fi
+
+
+echo "{\"names\":[]}" > ~/namesaver/names.json
